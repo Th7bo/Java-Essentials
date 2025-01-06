157e7f22129803ef45eabcabfd5dd1ad6032aaf1
ThiboFranssenPXL <Thibo.Franssen@student.pxl.be>
2025-01-06 18:59:59 +0100

    updates

--- H7/src/opdrachten/opdracht1/Test.java
+++ H7/src/opdrachten/opdracht1/Test.java
@@ -1,10 +1,27 @@
 package opdrachten.opdracht1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
-        Integer test = 5;
-        System.out.println(test.byteValue());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Geef een getal in:");
+        String input = sc.next();
+        Integer test = Integer.valueOf(input);
+        System.out.println(test);
+        test += 5;
+        System.out.println(test);
+        System.out.printf("Voor een Integer worden er %d bits gebruikt.%n", Integer.SIZE);
+        System.out.printf("Voor een Integer worden er %d bytes gebruikt.%n", Integer.BYTES);
+        // extra
+        ArrayList<Integer> lijst = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            lijst.add(i);
+        }
+        lijst.set(0, lijst.getFirst() * 5);
+        System.out.println(lijst);
     }
 
 }
--- H7/src/opdrachten/opdracht2/Faculteit.java
+++ H7/src/opdrachten/opdracht2/Faculteit.java
@@ -7,20 +7,18 @@ public class Faculteit {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(getFaculteit(BigInteger.valueOf(5000)));
+        BigInteger val = getFaculteit(BigInteger.valueOf(5000));
+        System.out.println(val);
+
         long endTime = System.currentTimeMillis();
         System.out.printf("Calculated in: %dms%n", endTime - startTime);
+        System.out.println("Cijfers: " + val.toString().length());
     }
-
-    private static final HashMap<BigInteger, BigInteger> cached = new HashMap<>();
 
     private static BigInteger getFaculteit(BigInteger a) {
         if (a.compareTo(BigInteger.ONE) <= 0) return a;
         BigInteger b = a.subtract(BigInteger.ONE);
-        if (cached.containsKey(b)) return cached.get(b);
-        BigInteger value = getFaculteit(b).multiply(a);
-        cached.put(b, value);
-        return value;
+        return getFaculteit(b).multiply(a);
     }
 
 }
