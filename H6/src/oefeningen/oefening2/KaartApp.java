867c906f52b2eed3dae1b6f10e3a2ed0e9f16e8d
ThiboFranssenPXL <Thibo.Franssen@student.pxl.be>
2025-01-09 10:30:00 +0100

    changes

--- H6/src/oefeningen/oefening2/KaartApp.java
+++ H6/src/oefeningen/oefening2/KaartApp.java
@@ -8,10 +8,13 @@ import java.util.Random;
 public class KaartApp {
 
     public static void main(String[] args) {
+        ArrayList<Kaart> original = new ArrayList<>();
         ArrayList<Kaart> kaart = new ArrayList<>();
         for (Soort soort : Soort.values()) {
             for (Waarde waarde : Waarde.values()) {
-                kaart.add(new Kaart(soort, waarde));
+                Kaart kaartje = new Kaart(soort, waarde);
+                kaart.add(kaartje);
+                original.add(kaartje);
             }
         }
 
@@ -19,10 +22,17 @@ public class KaartApp {
 
         Random rand = new Random();
         Kaart computerKaart = kaart.get(rand.nextInt(kaart.size()));
+        kaart.remove(computerKaart);
         Kaart spelerKaart = kaart.get(rand.nextInt(kaart.size()));
+        kaart.remove(spelerKaart);
         while (computerKaart.getWaarde().ordinal() == spelerKaart.getWaarde().ordinal()) {
+            kaart = new ArrayList<>(original);
+            Collections.shuffle(Arrays.asList(kaart));
+
             spelerKaart = kaart.get(rand.nextInt(kaart.size()));
+            kaart.remove(spelerKaart);
             computerKaart = kaart.get(rand.nextInt(kaart.size()));
+            kaart.remove(computerKaart);
             System.out.println("Geen winnaar, kies nieuwe kaarten.");
         }
         if (computerKaart.getWaarde().ordinal() > spelerKaart.getWaarde().ordinal()) {
